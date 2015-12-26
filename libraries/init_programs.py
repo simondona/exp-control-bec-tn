878a256c2a6f32425dcdcd3731cebee5772209c7
Simone Donadello <simon.dona.dev@gmail.com>
2015-12-27 00:02:51 +0100

    fix bug opening programs in root program folder

--- libraries/init_programs.py
+++ libraries/init_programs.py
@@ -3,17 +3,17 @@
 
 # Experiment Control BEC TN
 # Copyright (C) 2015  Simone Donadello
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -28,8 +28,8 @@ def program_list_init(action_list):
             fstart, fext = os.path.splitext(fname)
             if fext == ".py" and fstart != "__init__":
                 module = imp.load_source(fstart, os.path.join(dirpath, fname))
-                categories = dirpath.replace(programs_path+"/", "", 1).split("/")
-                categories = tuple(categories)
+                categories = dirpath.replace(programs_path, "", 1).split("/")
+                categories = tuple(cat for cat in categories if cat)
 
                 if "prg_comment" in vars(module):
                     prg_comment = module.prg_comment
